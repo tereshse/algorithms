7f6b4f5f309970b0219bf58d4cf440c583991b6f
Sergii Tereshchenko <sergey.tereschenko@gmail.com>
2012-11-10 14:17:59 +0200

    + Finish Dijkstra algo implementation

--- Dijkstra/src/dijkstra/Dijkstra.scala
+++ Dijkstra/src/dijkstra/Dijkstra.scala
@@ -15,13 +15,37 @@ object Dijkstra {
   }
 
   class Graph(adj: Array[Set[Edge]]) {
+    // Vertices count
     def V() = adj.size
+    // Edges count
     def E() = adj.foldLeft(0)((x, s) => x + s.size)
+    //get all edges
     def edges() = adj.foldLeft(Set[Edge]())((xs, s) => xs ++ s)
+    //get edges accessed from the vertex
     def edges(v: Vertex) = adj(v)
   }
 
-  class ShortestPath(val graph: Graph, val start: Vertex){
-    def pathTo(target: Vertex) : (Int, List[Vertex]) = (1,Nil)
+  class ShortestPath(val graph: Graph, val start: Vertex) {
+
+    def findShortestPathTo(target: Vertex): (Int, List[Vertex]) = {
+      if (target == start) (0, start :: Nil)
+      else findShortestPathToInternal(target, Set((start, 0)), (0, start :: Nil))
+    }
+
+    def findShortestPathToInternal(target: Vertex, explored: Set[(Vertex, Int)], path: (Int, List[Vertex])): (Int, List[Vertex]) = {
+      val newExplored = findVerticesAccessedFromExplored(explored).minBy(_._2)
+      if (newExplored._1 == target)
+        (newExplored._2, path._2 :+ newExplored._1)
+      else
+        findShortestPathToInternal(target, explored + newExplored, (newExplored._2, path._2 :+ newExplored._1))
+    }
+
+    def findVerticesAccessedFromExplored(explored: Set[(Vertex, Int)]): Set[(Vertex, Int)] = {
+      for (
+        exp <- explored;
+        edge <- graph.edges(exp._1) if explored.exists(p => p._1.equals(edge.from)) && !explored.exists(p => p._1.equals(edge.to))
+      ) yield (edge.to, exp._2 + edge.weight)
+    }
   }
+  
 }
--- Dijkstra/test/dijkstra/DijkstraSuite.scala
+++ Dijkstra/test/dijkstra/DijkstraSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import dijkstra.Dijkstra.Graph
 import dijkstra.Dijkstra.Edge
+import dijkstra.Dijkstra.ShortestPath
 
 @RunWith(classOf[JUnitRunner])
 class DijkstraSuite extends FunSuite {
@@ -36,5 +37,21 @@ class DijkstraSuite extends FunSuite {
 
     val edges_1 = graph.edges(1)
     assert(Set(new Edge(1, 3, 6), new Edge(1, 2, 2)).forall(p => edges_1.contains(p)))
+  }
+  
+  test("Shortest path calculation") {
+    val graph = new Graph(Array(
+      Set(new Edge(0, 1, 1), new Edge(0, 2, 4)),
+      Set(new Edge(1, 3, 6), new Edge(1, 2, 2)),
+      Set(new Edge(2, 3, 3)),
+      Set())) 
+
+    val path = new ShortestPath(graph, 0)
+    
+    assert( path.findShortestPathTo(3) === (6, List(0,1,2,3)))
+    assert( path.findShortestPathTo(0) === (0, List(0)))
+    assert( path.findShortestPathTo(2) === (3, List(0,1,2)))
+    
+    assert( new ShortestPath(graph, 1).findShortestPathTo(3) === (5, List(1,2,3)))
   }
 }
